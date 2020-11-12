e30607730cd93f523c3e0404a5d72feda6ed594e
Fabiano Traple <fabianotraple@gmail.com>
2020-11-12 14:17:49 -0300

    Create sound example.

--- examples/sound/sound-example.c
+++ examples/sound/sound-example.c
@@ -6,12 +6,12 @@
 
 int main() {
     // Create window
-    if (!pge_window_create("PGE - Music Example", 640, 480, 1, 60, false)) {
+    if (!pge_window_create("PGE - Sound Example", 640, 480, 1, 60, false)) {
         printf("%s\n", pge_get_error());
         return EXIT_FAILURE;
     }
-    // Load music
-    pge_MusicObj music = pge_music_load("../../../examples/resources/music/gs-16b-2c-44100hz.mp3");
+    // Load sound
+    pge_SoundObj music = pge_sound_load("../../../examples/resources/sound/power_pellet.wav");
     if (!music) {
         printf("%s\n", pge_get_error());
         return EXIT_FAILURE;
@@ -19,34 +19,34 @@ int main() {
     // Game loop
     while (pge_window_is_running()) {
         pge_window_clear(PGE_COLOR_BLACK);
-        // Select Music
+        // Select Sound
         pge_draw_set_current_color(PGE_COLOR_BROWN);
         pge_draw_text(10, 10, 2, "Press Button:\nA = play   B = stop\nX = pause  Y = resume");
         if (pge_controller_button(0, PGE_CONTROLLER_BUTTON_A) &&
-            pge_music_status(music) == PGE_AUDIO_STATUS_STOPED) {
+            pge_sound_status(music) == PGE_AUDIO_STATUS_STOPED) {
             printf("PGE_CONTROLLER_BUTTON_A - play\n");
-            pge_music_play(music, true);
+            pge_sound_play(music, true);
         }
         if (pge_controller_button(0, PGE_CONTROLLER_BUTTON_B) &&
-            pge_music_status(music) == PGE_AUDIO_STATUS_PLAYING) {
+            pge_sound_status(music) == PGE_AUDIO_STATUS_PLAYING) {
             printf("PGE_CONTROLLER_BUTTON_B - stop\n");
-            pge_music_stop(music);
+            pge_sound_stop(music);
         }
         if (pge_controller_button(0, PGE_CONTROLLER_BUTTON_X) &&
-            pge_music_status(music) == PGE_AUDIO_STATUS_PLAYING) {
+            pge_sound_status(music) == PGE_AUDIO_STATUS_PLAYING) {
             printf("PGE_CONTROLLER_BUTTON_X - pause\n");
-            pge_music_pause(music);
+            pge_sound_pause(music);
         }
         if (pge_controller_button(0, PGE_CONTROLLER_BUTTON_Y) &&
-            pge_music_status(music) == PGE_AUDIO_STATUS_PAUSED) {
+            pge_sound_status(music) == PGE_AUDIO_STATUS_PAUSED) {
             printf("PGE_CONTROLLER_BUTTON_Y - resume\n");
-            pge_music_resume(music);
+            pge_sound_resume(music);
         }
         // Update window
         pge_window_show();
     }
     // Destroy
-    pge_music_unload(music);
+    pge_sound_unload(music);
     pge_window_destroy();
     return EXIT_SUCCESS;
 }
